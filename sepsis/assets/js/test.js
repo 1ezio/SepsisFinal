70f1f1cfd9d7fa6ccd730f0893a1834be7c4603a
ayush08642 <56427611+ayush08642@users.noreply.github.com>
2020-01-22 10:44:48 +0530

    Prevention page updated
    ;D

---
+++ sepsis/assets/js/test.js
@@ -0,0 +1,19 @@
+/*var age = document.getElementsByName("Age");
+var hr = document.getElementsByName("HR");
+var o2 = document.getElementsByName("O2Sat");
+var temp = document.getElementsByName("Temp");
+var sbp = document.getElementsByName("SBP");
+var dbp = document.getElementsByName("DBP");
+var res = document.getElementsByName("Resp");
+*/
+var class1 = "form-control p-2";
+
+document.getElementsById("id_Age").className = "form-control p-2";
+document.getElementsByName("HR").className = class1;
+document.getElementsByName("O2Sat").className = class1;
+document.getElementsByName("Temp").className = class1;
+document.getElementsByName("SBP").className = class1;
+document.getElementsByName("DBP").className = class1;
+document.getElementsByName("Resp").className = class1;
+document.getElementsByName("Resp").style.width = 90%;
+
